1989c9283adbb696e21d8c02d5b717bfe03ce34e
Estebanr32 <juanes020609@gmail.com>
2025-09-17 17:30:54 -0500

    final

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
 nombre = "Geezydee"
-nombre = "Jaco"
-nombre = "Daniel"
-pais = "gordola"
-joseando = "cAMELLANDING"
-print("Hola, Soy  turylalein!")
-print("Hola, Soy Geezydee")
-print("Hola, Soy Kris Rrrr")
 
-print("HOLA, bbcitaaaa" + nombre)
-print(1+1)
-print("Jorgee")
-print ("sisabes")
-print("HOLA, bbcitaaaa " + nombre)
-print("Hola, Soy  turylalein!")
-print("HOLA, bbcitaaaa " + nombre)
 
