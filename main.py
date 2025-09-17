2c1628a9b8153572c25d8cdd4c811fd4d86e4f0d
xq25 <jacoboq92@gmail.com>
2025-09-17 17:22:08 -0500

    Hola negro

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("Hola, Soy Geezydee")
 print("Hola, Soy Kris Rrrr")
 
 print("HOLA, bbcitaaaa" + nombre)
-
+print(1+1)
 print("Jorgee")
 print("HOLA, bbcitaaaa " + nombre)
 print("Hola, Soy  turylalein!")
